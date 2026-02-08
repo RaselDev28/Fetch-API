cac9a459d91c64fda2307800c2012faa85b7480a
RaselDev28 <rasel.dev28@gmail.com>
2026-02-08 20:14:46 +0600

    fetch

---
+++ script.js
@@ -0,0 +1,21 @@
+const loadData = () => {
+    const url = "https://jsonplaceholder.typicode.com/todos/1";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+const loadPosts = () => {
+    const url2 = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url2).then(res => res.json())
+        .then(data => {
+            console.log(data)
+            displayPost(data)
+        })
+}
+
+const displayPost = (posts) => {
+    posts.forEach(post => {
+        console.log(post)
+    });
+}
