f45e8f46c8f3b76bf7871b3af0e8f4fa5b172a96
RaselDev28 <rasel.dev28@gmail.com>
2026-02-08 22:13:58 +0600

    post

--- post.js
+++ post.js
@@ -8,12 +8,33 @@ const loadPosts=()=>{
     })
 }
 
+// const displayData=(posts)=>{
+//     const postContainer=document.getElementById("post-container");
+//     postContainer.innerHTML="";
+
+//     posts.forEach(post => {
+//         const li=document.createElement('li');
+//         li.innerText=post.title;
+//         postContainer.appendChild(li)
+//     });
+// }
+//     {
+// "userId": 1,
+// "id": 1,
+// "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+// "body": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+// },
+
 const displayData=(posts)=>{
     const postContainer=document.getElementById("post-container");
+    postContainer.innerHTML="";
 
     posts.forEach(post => {
-        const li=document.createElement('li');
-        li.innerText=post.title;
-        postContainer.appendChild(li)
+        const postCard=document.createElement("div");
+        postCard.innerHTML=`<div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>`
+        postContainer.appendChild(postCard)
     });
 }
